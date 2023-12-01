50fcb9ec4764bc8bc963dcafe2d72c400c209d78
kkznch <kkznch710md@gmail.com>
2023-12-01 15:45:20 +0900

    カードで良い感じに表示するようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
 import { FC } from 'react';
 import { useRecipes } from './hooks/useRecipes';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 const App: FC = () => {
   const { recipes } = useRecipes();
   return (
     <>
       <h1>shadcn使うよ</h1>
-      <div>{recipes.map((item) => item.title)}</div>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((item) => (
+          <Card key={item.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <div>
+                <CardTitle>{item.title}</CardTitle>
+                <CardDescription>{item.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{item.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {item.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </>
   );
 };
